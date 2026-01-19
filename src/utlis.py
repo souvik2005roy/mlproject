a7680a15858c27deb9e4afd477d2d3450d11eea4
Souvik Roy <souvik.roy0946@gmail.com>
2026-01-19 18:44:18 +0530

    Pipeline & Deployment

--- src/utlis.py
+++ src/utlis.py
@@ -45,3 +45,10 @@ def evaluate_models(x_train, y_train, X_test, y_test, models,param=None):
         return report
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path: str):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
